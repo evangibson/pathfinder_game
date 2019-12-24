6216c4a148061a3afcffb52c56309cb5fa712864
Evan Gibson <evangibson2794@gmail.com>
2019-12-24 15:30:56 -0500

    add wall and blank argument to __init__

--- maze_obj.py
+++ maze_obj.py
@@ -3,14 +3,22 @@ import numpy as np
 class MazeMap:
     """A Class that establishes a maze's environment"""
 
-    def __init__(self, dimensions):
+    def __init__(self,
+                 dimensions,
+                 wall = -1,
+                 blank = 0):
         # Tuple argument for the length and width of the maze
         self.size = dimensions
 
         # By default, maze will generate as blank
         self.maze = np.zeros(self.size, dtype=int)
 
-    def random_walls(self, prop_of_walls, wall_value=-1):
+        # Declare wall and blank space values
+        self.wall_value = wall
+        self.blank_value = blank
+
+
+    def random_walls(self, prop_of_walls):
         """Builds walls within a maze based on randomization parameters"""
 
         # Map random indices for wall placement
@@ -21,7 +29,7 @@ class MazeMap:
         temp_arr = self.maze.flatten()
 
         # Remake original structure
-        temp_arr[indices] = wall_value
+        temp_arr[indices] = self.wall_value
 
         # Remake original structure
         self.maze = temp_arr.reshape(self.size)
