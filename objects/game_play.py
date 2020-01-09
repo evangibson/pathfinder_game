98149ac9f48566de4d27d4441656dda10bc1fa94
Evan Gibson <evangibson2794@gmail.com>
2020-01-08 22:18:06 -0500

    added objects folder and moved game, maze, and player objects

---
+++ objects/game_play.py
@@ -0,0 +1,80 @@
+import maze_obj
+import player_obj
+
+
+class MazeGame:
+    def __init__(self, MazeMap_obj):
+        """Works with a MazeMap obj with player positions already assigned"""
+        self.play_map = MazeMap_obj
+        self.players = dict()
+
+        # Create a player object for each uid
+        for id in self.play_map.player_positions:
+            # Run instantiation actions
+            temp_player = player_obj.MazeRunner()
+            temp_player.pres_pos = self.play_map.player_positions[id][0]
+            self.players.update({id: temp_player})
+
+
+    def check_move(nonsense_arg):
+        """Currently functional but needs fine-tuning"""
+
+        pass_val = False
+
+        # Zero bounds check
+        if nonsense_arg[0] >= 0 or nonsense_arg[1] >= 0:
+            pass_val = True
+        else:
+            pass
+
+        # Edges of play area check
+        if nonsense_arg[0] < test_game.play_map.dims[0] - 1 or nonsense_arg[1] < test_game.play_map.dims[1] - 1:
+            pass_val = True
+        else:
+            pass
+
+            # Blank value check
+        if test_game.play_map.maze[nonsense_arg] is test_game.play_map.blank_value:
+            pass_val = True
+        else:
+            pass
+
+        return pass_val
+
+    # Request action from each player
+    def action_request(mazerunner, maze_obj_, allow_spaces=self.maze_obj_.blank_value):
+
+        """UNSTABLE"""
+
+        first_pos = mazerunner.pres_pos
+
+        # Start by requesting move actions
+
+        # Action accepted
+        accept = False
+        while accept is False:
+            mazerunner.move_action(input("Move Direction: "))
+
+            # check value at proposed position
+            if maze_obj_.maze[mazerunner.pres_pos] in allow_spaces:
+
+                maze_obj.maze
+                accept = True
+
+            else:
+                continue
+
+
+
+def main(human_game = False):
+    if human_game is True:
+        # Memory saving step. Human games will not feel extra imports as heavily as ML games
+        import time
+
+        # Spacesaver for human games stepping
+
+    else:
+        print("Spacesaver")
+
+if __name__ == "__main__":
+    main()
---
+++ objects/maze_obj.py
@@ -0,0 +1,122 @@
+import numpy as np
+import sys
+
+
+class MazeMap:
+    """A Class that establishes a maze's environment"""
+
+    def __init__(self,
+                 dimensions,
+                 wall=-1,
+                 blank=0):
+        # Tuple argument for the length and width of the maze
+        self.dims = dimensions
+
+        # By default, maze will generate as blank
+        self.maze = np.zeros(self.dims, dtype=int)
+
+        # Declare wall and blank space values
+        self.wall_value = wall
+        self.blank_value = blank
+
+        # Storage for the wall indices
+        self.wall_indices = None
+
+        # RESERVED non-uid values. Will help from confusing uids with other value types. Designed to be added to
+        self.black_list = list([self.wall_value, self.blank_value])
+
+        # Default uid (meant to be iterated sequentially)
+        self.next_play_id = 101
+
+        # Indices that players can't be placed in
+        self.forbid_indices = list()
+
+        self.player_positions = dict()
+
+    def random_walls(self, prop_of_walls):
+        """Builds walls within a maze based on randomization parameters"""
+
+        # Map random indices for wall placement
+        self.wall_indices = np.random.choice(np.arange(self.maze.size),
+                                             replace=False,
+                                             size=int(self.maze.size * prop_of_walls))
+
+        # Add walls to forbidden zones
+        self.forbid_indices.append(list(self.wall_indices))
+
+        # Create flattened array to map
+        temp_arr = self.maze.flatten()
+
+        # Remake original structure
+        temp_arr[self.wall_indices] = self.wall_value
+
+        # Remake original structure
+        self.maze = temp_arr.reshape(self.dims)
+
+        return self.maze
+
+    def place_player(self, start_position=None, uid=None):
+        """Will randomly select a player starting position and uid if both arguments are None.
+        PLEASE NOTE: Does not actually assign a MazeRunner object to a position. It merely assigns a UID and start point"""
+        if start_position is None:
+            temp_player_index = np.random.choice(np.arange(self.maze.size),
+                                                 replace=False,
+                                                 size=1)
+            # Create flattened array to map
+            temp_arr = self.maze.flatten()
+
+            # Provide stop placeholder for loop
+            loop_brake = 0
+
+            while int(temp_player_index) in self.forbid_indices[0]:
+                # Propose index for player to occupy
+                temp_player_index = np.random.choice(np.arange(self.maze.size),
+                                                     replace=False,
+                                                     size=1)
+
+                # To keep the placeholder loop from running to infinity
+                loop_brake += 1
+                if loop_brake > len(temp_arr):
+                    sys.exit("Player Placement Failed! No unreserved space detected")
+                else:
+                    continue
+
+            if uid is None:
+                # Remake original structure
+                temp_arr[temp_player_index] = self.next_play_id
+
+                # Carry progress to values list
+                self.black_list.append(self.next_play_id)
+
+            elif uid not in self.black_list:
+                temp_arr[temp_player_index] = uid
+
+                # Carry progress to values list
+                self.black_list.append(uid)
+
+            else:
+                sys.exit("Player Placement Failed! Proposed UID already in black_list")
+
+            self.forbid_indices[0].append(int(temp_player_index))
+
+            # Remake original structure
+            self.maze = temp_arr.reshape(self.dims)
+
+        if uid is None:
+            self.find_player(self.next_play_id)
+            self.next_play_id += 1
+        else:
+            self.find_player(uid)
+
+    def find_player(self, uid, silent = True):
+        """Can function to find multi-id coordinates, too"""
+        full_loc_list = list()
+
+        for i in range(0, len(np.where(self.maze == uid)[0])):
+            player_loc = list([np.where(self.maze == uid)[0][i], np.where(self.maze == uid)[1][i]])
+            full_loc_list.append(player_loc)
+
+        self.player_positions.update({uid: full_loc_list})
+
+        if silent is False:
+            return dict({uid: full_loc_list})
